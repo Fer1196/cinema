8d781f05b705bd23642d1076afce5288fae9577b
Fer1196 <rodifer1196@hotmail.com>
2023-12-04 17:22:33 -0500

    Movies List

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { MovieCatalog } from "./components/gridMovies";
+import { Movie } from "./components/movie";
+import { moviesCatalog } from "./utils/moviesCatalog";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header>PELIS FREE</header>
+      <MovieCatalog movieList={moviesCatalog}></MovieCatalog>
+      <footer>CINEMA -FER</footer>
+    </>
   );
 }
 
